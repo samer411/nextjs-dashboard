0cda5a48b8ac696a5531e9b22946d7e4611c2561
Samer Nassim <74414236+samer411@users.noreply.github.com>
2024-11-24 04:02:53 +0200

    initial commit

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,19 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
-import { notFound } from 'next/navigation';
-
+ 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
-    const params = await props.params;
-    const id = params.id;
-    const [invoice, customers] = await Promise.all([
-        fetchInvoiceById(id),
-        fetchCustomers(),
-    ]); 
-    if(!invoice){
-      notFound();
-    }
-    return (
+  const params = await props.params; 
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),])
+     return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
